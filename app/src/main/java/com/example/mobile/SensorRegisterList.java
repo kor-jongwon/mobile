6a1c727208f011bb48a396c31457f7e0d9bea3a5
유진 <102151383+youj0912@users.noreply.github.com>
2023-11-08 19:00:10 +0900

    Sensor_register_list 레이아웃 구현

---
+++ app/src/main/java/com/example/mobile/SensorRegisterList.java
@@ -0,0 +1,15 @@
+package com.example.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SensorRegisterList extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sensor_register_list); // 레이아웃 파일의 이름에 맞게 변경해야 합니다.
+
+        // 여기에 화면 초기화 및 기타 동작을 설정할 수 있습니다.
+    }
+}

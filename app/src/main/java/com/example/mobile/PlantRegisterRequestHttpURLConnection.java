852fa8c562d583642f60c7d3836a701313e61413
danhuichoi <eodrnf0123@naver.com>
2023-11-29 16:10:20 +0900

    fix:[등록]: 이미지 등록 변환

--- app/src/main/java/com/example/mobile/PlantRegisterRequestHttpURLConnection.java
+++ app/src/main/java/com/example/mobile/PlantRegisterRequestHttpURLConnection.java
@@ -1,5 +1,11 @@
 package com.example.mobile;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
--- app/src/main/java/com/example/mobile/RegisterPlantActivity.java
+++ app/src/main/java/com/example/mobile/RegisterPlantActivity.java
@@ -5,6 +5,7 @@ import android.app.DatePickerDialog;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -43,7 +44,7 @@ public class RegisterPlantActivity extends Activity {
     private Button btnSave;
     private ImageView imageView;
     private boolean isImageSelected = false;
-    private boolean isDuplicateName = false;
+    private boolean isNameAvailable = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +58,7 @@ public class RegisterPlantActivity extends Activity {
         Button duplicateButton = findViewById(R.id.duplicateButton);
         btnSave = findViewById(R.id.button);
 
-        btnSave.setEnabled(false); //버튼 비활성화 상태
+        btnSave.setEnabled(false); // 초기 버튼 비활성화 상태
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,7 +72,7 @@ public class RegisterPlantActivity extends Activity {
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                validateInputs(); // 텍스트가 변경될 때 확인
+                validateInputs();
             }
             @Override
             public void afterTextChanged(Editable s) {}
@@ -92,11 +93,10 @@ public class RegisterPlantActivity extends Activity {
             }
         });
 
-        // "완료" 버튼 클릭 리스너를 설정합니다.
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                savePlantData(); // 사용자 데이터 저장 메소드를 호출합니다.
+                savePlantData();
             }
         });
     }
@@ -113,30 +113,23 @@ public class RegisterPlantActivity extends Activity {
                     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                         String selectedDate = year + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
                         textViewDate.setText(selectedDate);
-                        validateInputs(); // 날짜가 설정된 후 검증
+                        validateInputs();
                     }
                 }, year, month, day);
         datePickerDialog.show();
     }
 
     public void openGallery() {
-        // Toast 메시지 추가
         Toast.makeText(this, "이미지를 선택하세요.", Toast.LENGTH_SHORT).show();
-
-        // 갤러리를 여는 인텐트 생성
         Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-        // 결과를 반환받기 위해 액티비티 시작
         startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
             Uri uri = data.getData();
-
             try {
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
                 imageView.setImageBitmap(bitmap);
@@ -144,37 +137,34 @@ public class RegisterPlantActivity extends Activity {
                 validateInputs();
             } catch (IOException e) {
                 e.printStackTrace();
-                Toast.makeText(this, "Failed to load image", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "이미지 로드 실패", Toast.LENGTH_LONG).show();
             }
         }
     }
 
     private void validateInputs() {
-        // 모든 입력 데이터가 유효한지 확인합니다.
-        // 그렇지 않으면 비활성화합니다.
-        btnSave.setEnabled(isImageSelected && isDuplicateName &&!TextUtils.isEmpty(editTextPlantName.getText().toString().trim()) && !TextUtils.isEmpty(textViewDate.getText()));
-        // 모든 조건이 충족되면 버튼을 활성화합니다.
+        btnSave.setEnabled(isImageSelected && isNameAvailable && !TextUtils.isEmpty(editTextPlantName.getText().toString().trim()) && !TextUtils.isEmpty(textViewDate.getText()));
     }
 
     private void savePlantData() {
         String plantName = editTextPlantName.getText().toString();
         String plantDate = textViewDate.getText().toString();
 
-        // Map으로 데이터를 저장합니다.
-        Map<String, String> values = new HashMap<>();
-        values.put("plantName", plantName);
-        values.put("plantingDate", plantDate);
+        // imageView에서 Bitmap 가져오기
+        Bitmap bitmap = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
 
-        // 이미지 데이터 준비
-        imageView.setDrawingCacheEnabled(true);
-        Bitmap bitmap = imageView.getDrawingCache();
+        // Bitmap을 byte 배열로 변환
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bos);
         byte[] imageData = bos.toByteArray();
-        imageView.setDrawingCacheEnabled(false);
 
-        String url = api_url.REGISTPLANT.getValue(); // 서버 URL을 설정해야 합니다.
-        NetworkTask networkTask = new NetworkTask(url, values, imageData, "plant_image.jpg");
+        String url = api_url.REGISTPLANT.getValue();
+
+        Map<String, String> data = new HashMap<>();
+        data.put("plantName", plantName);
+        data.put("plantingDate", plantDate);
+
+        NetworkTask networkTask = new NetworkTask(url, data, imageData, "plant_image.jpg");
         networkTask.execute();
     }
 
@@ -182,24 +172,22 @@ public class RegisterPlantActivity extends Activity {
     public class NetworkTask extends AsyncTask<Void, Void, String> {
 
         private String url;
-        private Map<String, String> values;
+        private Map<String, String> params;
         private byte[] imageData;
         private String imageName;
 
-        // 생성자에서 이미지 데이터와 파일 이름을 추가로 받습니다.
-        public NetworkTask(String url, Map<String, String> values, byte[] imageData, String imageName) {
+        public NetworkTask(String url, Map<String, String> params, byte[] imageData, String imageName) {
             this.url = url;
-            this.values = values;
+            this.params = params;
             this.imageData = imageData;
             this.imageName = imageName;
         }
 
         @Override
-        protected String doInBackground(Void... params) {
+        protected String doInBackground(Void... voids) {
             PlantRegisterRequestHttpURLConnection requestHttpURLConnection = new PlantRegisterRequestHttpURLConnection();
             try {
-                // 수정된 request 메서드를 호출합니다. 이 메서드는 이미지와 텍스트 데이터를 모두 서버로 전송합니다.
-                return requestHttpURLConnection.request(url, values, imageData, imageName);
+                return requestHttpURLConnection.request(url, params, imageData, imageName);
             } catch (IOException e) {
                 e.printStackTrace();
                 return null;
@@ -207,22 +195,18 @@ public class RegisterPlantActivity extends Activity {
         }
 
         @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-
-            // 결과 처리
-            if (s != null) {
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (result != null && !result.isEmpty()) {
                 try {
-                    JSONObject jsonObject = new JSONObject(s);
+                    JSONObject jsonObject = new JSONObject(result);
                     String message = jsonObject.optString("message", "");
 
                     if (message.contains("식물 정보가 성공적으로 추가되었습니다.")) {
-                        // 다음 액티비티로 이동
                         Intent intent = new Intent(RegisterPlantActivity.this, Plant_RegisterList.class);
                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
                         startActivity(intent);
                     } else {
-                        // 오류 메시지 처리
                         Toast.makeText(getApplicationContext(), "등록 실패: " + message, Toast.LENGTH_LONG).show();
                     }
                 } catch (JSONException e) {
